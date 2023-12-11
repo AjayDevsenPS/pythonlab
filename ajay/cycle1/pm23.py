3125cad000abade522bc9d85614c0ba5c05cbc8e
AjayDevsenPS <147644760+AjayDevsenPS@users.noreply.github.com>
2023-12-11 11:21:08 +0530

    Add files via upload

--- ajay/cycle1/pm23.py
+++ ajay/cycle1/pm23.py
@@ -1,13 +1,17 @@
-numbers=[-3,5,-8,10,-2,15]
+numbers=[]
+limit=int(input("Enter number of elements:"))
+for i in range(0,limit):
+	ele=int(input())
+	numbers.append(ele)
 positive_numbers=[num for num in numbers if num>0]
 print("The list is",numbers)
 print("The positive numbers in the list are",positive_numbers)
-n=5
 squares=[x**2 for x in numbers]
 print("The squares of the numbers in list are",squares)
-word="hello"
+word=input("Enter the word ")
 vowels=[char for char in word if char in 'aeiouAEIOU']
 print("The character is ",word)
 print("The vowels in the character are",vowels)
-ordinal_values=[ord(char) for char in word]
-print("The ordinal values are",ordinal_values)
+print("The ordinal values of each characters are:")
+for char in word:
+	print(ord(char))
