748dbce28455731c49dec21dd735af5e612f45bd
AjayDevsenPS <147644760+AjayDevsenPS@users.noreply.github.com>
2023-12-14 21:57:28 +0530

    Update pm8.py

--- ajay/cycle4/pm8.py
+++ ajay/cycle4/pm8.py
@@ -14,4 +14,4 @@ def series_sum(n):
 n = int(input("Enter the number of terms in the series: "))
 
 result = series_sum(n)
-print("Sum of the series 1/1!+4/4!+21/3!+..to the",n,"th term is:", result)
+print("Sum of the series 1/1!+4/2!+21/3!+..to the",n,"th term is:", result)
